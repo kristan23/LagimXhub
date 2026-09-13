ce0ff8ad1c5b7ae48b17c5e62facc48ecd0d93b8
kristan23 <kristanh15@gmail.com>
2026-09-13 21:48:41 +0800

    Update BeastHub references and label styles

--- adobo.lua
+++ adobo.lua
@@ -1,13 +1,13 @@
 if not getgenv().BeastHubRayfield then
     getgenv().BeastHubRayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 end
-local Rayfield = getgenv().RyuHubXRayfield
+local Rayfield = getgenv().BeastHubRayfield
 local beastHubIcon = 109838189843903
 
 -- local isVerified = ...  -- grab the passed value
 local isVerified = getgenv()._bh_isVerified
 local expiryText=isVerified==true and "lifetime" or (isVerified and tostring(isVerified) or "Lifetime")
-local scriptTitle = "RyuHub | Exp: "..expiryText
+local scriptTitle = "BeastHubXDevsHub | Exp: "..expiryText
 if getgenv().BeastHubLoaded then
     if Rayfield then
         Rayfield:Notify({
@@ -2968,22 +2968,22 @@ local Toggle_bhubESP = PetEggs:CreateToggle({
 
                                 if isHuge then
                                     if rawKG < 5 then
-                                        label.Text = '<font color="rgb(255,0,0)"><b>ARAY KO!</b></font>\n<font color="rgb(255,0,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
+                                        label.Text = '<font color="rgb(255,0,0)"><b>ARAY KO!</b></font>\n<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
                                     elseif rawKG < 8 then
                                         local brontoKG = string.format("%.2f", rawKG * 1.3)
-                                        label.Text = '<font color="rgb(255,0,0)"><b>PALDO! ('..brontoKG..'kg)</b></font>\n<font color="rgb(255,0,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
+                                        label.Text = '<font color="rgb(255,0,0)"><b>PALDO! ('..brontoKG..'kg)</b></font>\n<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
                                     else
                                         local brontoKG = string.format("%.2f", rawKG * 1.3)
-                                        label.Text = '<font color="rgb(255,0,0)"><b>PALDOOOOO!!! ('..brontoKG..'kg)</b></font>\n<font color="rgb(255,0,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
+                                        label.Text = '<font color="rgb(255,0,0)"><b>PALDOOOOO!!! ('..brontoKG..'kg)</b></font>\n<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
                                     end
                                 else
                                     label.Text = '<font color="rgb(255,0,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
                                 end
 
-                                label.TextColor3 = Color3.fromRGB(255,0,0)
+                                label.TextColor3 = Color3.fromRGB(204, 204, 0)
                                 label.TextStrokeTransparency = 0.5
                                 label.TextScaled = false
-                                label.TextSize = 15
+                                label.TextSize = 18
                                 label.Font = Enum.Font.SourceSans
                                 label.Parent = billboard
                             end
