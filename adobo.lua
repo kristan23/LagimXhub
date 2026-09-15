b9a826c4ead3ee0a3362c7db6e848b48e50e04d8
kristan23 <kristanh15@gmail.com>
2026-09-15 21:40:26 +0800

    Change label text color and size in adobo.lua

--- adobo.lua
+++ adobo.lua
@@ -2976,10 +2976,10 @@ local Toggle_bhubESP = PetEggs:CreateToggle({
                                     label.Text = '<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
                                 end
 
-                                label.TextColor3 = Color3.fromRGB(0, 0, 0)
+                                label.TextColor3 = Color3.fromRGB(50, 205, 50)
                                 label.TextStrokeTransparency = 0.5
                                 label.TextScaled = false
-                                label.TextSize = 12
+                                label.TextSize = 17
                                 label.Font = Enum.Font.SourceSans
                                 label.Parent = billboard
                             end
