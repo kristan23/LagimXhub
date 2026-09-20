736939c3dd46636cd515ab3a355fe336cd1ee3ed
DevNamedJowao <kristanh15@gmail.com>
2026-09-20 16:10:50 +0800

    Change label text size from 17 to 15

--- adobo.lua
+++ adobo.lua
@@ -3125,7 +3125,7 @@ local Toggle_bhubESP = PetEggs:CreateToggle({
                                 label.TextColor3 = Color3.fromRGB(255,0,0)
                                 label.TextStrokeTransparency = 0.5
                                 label.TextScaled = false
-                                label.TextSize = 17
+                                label.TextSize = 15
                                 label.Font = Enum.Font.SourceSans
                                 label.Parent = billboard
                             end
