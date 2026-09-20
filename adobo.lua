338194f4ffb92fc5bf3f1ac7e3698993368186b4
DevNamedJowao <kristanh15@gmail.com>
2026-09-20 19:30:33 +0800

    Change label text from 'TAINA BUGOK!' to 'TADOOOO!'

--- adobo.lua
+++ adobo.lua
@@ -3110,7 +3110,7 @@ local Toggle_bhubESP = PetEggs:CreateToggle({
 
                                 if isHuge then
                                     if rawKG < 5 then
-                                        label.Text = '<font color="rgb(0,255,255)"><b>TAINA BUGOK!</b></font>\n<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
+                                        label.Text = '<font color="rgb(0,255,255)"><b>TADOOOO!</b></font>\n<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
                                     elseif rawKG < 8 then
                                         local brontoKG = string.format("%.2f", rawKG * 1.3)
                                         label.Text = '<font color="rgb(0,255,255)"><b>DINOROBONG TUBO! ('..brontoKG..'kg)</b></font>\n<font color="rgb(204,204,0)"><b>' .. petName .. '</b></font><b> = ' .. petKG .. 'kg</b>'
