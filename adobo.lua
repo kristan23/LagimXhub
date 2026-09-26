cc868a0ba06c1124d64acd7bf7a8b55e5000377e
DevNamedJowao <kristanh15@gmail.com>
2026-09-26 20:22:46 +0800

    Adjust eggOffsets for Right - Z Shape positioning

--- adobo.lua
+++ adobo.lua
@@ -927,22 +927,21 @@ local function getPositionForPlaceEggs()
         
  elseif positionForPlaceEggs == "Right - Z Shape" then
         eggOffsets = {
-            Vector3.new(19, 0, -33),
-            Vector3.new(24, 0, -33),
-            Vector3.new(29, 0, -33),
-            Vector3.new(34, 0, -33),
-            Vector3.new(39, 0, -33),
+Vector3.new(19, 0, -34),
+Vector3.new(24, 0, -34),
+Vector3.new(29, 0, -34),
+Vector3.new(34, 0, -34),
+Vector3.new(39, 0, -34),
 
-            Vector3.new(34, 0, -38),
-            Vector3.new(29, 0, -43),
-            Vector3.new(24, 0, -48),
+Vector3.new(34, 0, -39),
+Vector3.new(29, 0, -44),
+Vector3.new(24, 0, -49),
 
-            Vector3.new(19, 0, -53),
-
-            Vector3.new(19, 0, -63),
-            Vector3.new(24, 0, -63),
-            Vector3.new(29, 0, -63),
-            Vector3.new(34, 0, -63),
+Vector3.new(19, 0, -54),
+Vector3.new(24, 0, -54),
+Vector3.new(29, 0, -54),
+Vector3.new(34, 0, -54),
+Vector3.new(39, 0, -54),
         }
 
     elseif positionForPlaceEggs == "Left - Z Shape" then
