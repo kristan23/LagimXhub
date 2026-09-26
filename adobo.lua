d1fc277cc932ef3cd59eb56a8810c4f6faed4460
DevNamedJowao <kristanh15@gmail.com>
2026-09-26 20:27:41 +0800

    Update Vector3 coordinates in adobo.lua

--- adobo.lua
+++ adobo.lua
@@ -933,9 +933,9 @@ Vector3.new(29, 0, -34),
 Vector3.new(34, 0, -34),
 Vector3.new(39, 0, -34),
 
-Vector3.new(34, 0, -39),
-Vector3.new(29, 0, -44),
-Vector3.new(24, 0, -49),
+Vector3.new(19, 0, -39),
+Vector3.new(24, 0, -44),
+Vector3.new(29, 0, -49),
 
 Vector3.new(19, 0, -54),
 Vector3.new(24, 0, -54),
